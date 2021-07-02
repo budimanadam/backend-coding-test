95424450e4aa22403ef98b9d9fa9486020a0ede0
Muhamad Adam Budiman <muhamadadambudiman@onoff.insure>
2021-07-02 20:16:24 +0700

    add implement tooling

---
+++ src/app.js
@@ -0,0 +1,160 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
+
+const winston = require('winston');
+
+const logger = winston.createLogger({
+    defaultMeta: {service: 'user-service'},
+    format: winston.format.json(),
+    level: 'info',
+    transports: [
+        
+        /*
+         * 
+         * - Write all logs with level `error` and below to `error.log`
+         * - Write all logs with level `info` and below to `combined.log`
+         * 
+         */
+        new winston.transports.File({filename: 'error.log', 
+            level: 'error'}),
+        new winston.transports.File({filename: 'combined.log'})
+    ]
+});
+
+/*
+ * 
+ * If we're not in production then log to the `console` with the format:
+ * `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
+ * 
+ */
+if (process.env.NODE_ENV !== 'production') {
+    logger.add(new winston.transports.Console({
+        format: winston.format.simple()
+    }));
+}
+
+module.exports = (db) => {
+    app.get('/health', (req, res) => {
+        console.log('aaa');
+        res.send('Healthy');
+    });
+
+    app.post('/rides', jsonParser, (req, res) => {
+        const startLatitude = Number(req.body.start_lat);
+        const startLongitude = Number(req.body.start_long);
+        const endLatitude = Number(req.body.end_lat);
+        const endLongitude = Number(req.body.end_long);
+        const riderName = req.body.rider_name;
+        const driverName = req.body.driver_name;
+        const driverVehicle = req.body.driver_vehicle;
+
+        if (startLatitude < -90 || startLatitude > 90 || startLongitude < -180 || startLongitude > 180) {
+            return res.send({
+                error_code: 'VALIDATION_ERROR',
+                message: 'Start latitude and longitude must be between -90 - 90 and -180 to 180 degrees respectively'
+            });
+        }
+
+        if (endLatitude < -90 || endLatitude > 90 || endLongitude < -180 || endLongitude > 180) {
+            return res.send({
+                error_code: 'VALIDATION_ERROR',
+                message: 'End latitude and longitude must be between -90 - 90 and -180 to 180 degrees respectively'
+            });
+        }
+
+        if (typeof riderName !== 'string' || riderName.length < 1) {
+            return res.send({
+                error_code: 'VALIDATION_ERROR',
+                message: 'Rider name must be a non empty string'
+            });
+        }
+
+        if (typeof driverName !== 'string' || driverName.length < 1) {
+            return res.send({
+                error_code: 'VALIDATION_ERROR',
+                message: 'Rider name must be a non empty string'
+            });
+        }
+
+        if (typeof driverVehicle !== 'string' || driverVehicle.length < 1) {
+            return res.send({
+                error_code: 'VALIDATION_ERROR',
+                message: 'Rider name must be a non empty string'
+            });
+        }
+
+        var values = [req.body.start_lat, req.body.start_long, req.body.end_lat, req.body.end_long, req.body.rider_name, req.body.driver_name, req.body.driver_vehicle];
+        
+        const result = db.run('INSERT INTO Rides(startLat, startLong, endLat, endLong, riderName, driverName, driverVehicle) VALUES (?, ?, ?, ?, ?, ?, ?)', values, function (err) {
+            if (err) {
+                return res.send({
+                    error_code: 'SERVER_ERROR',
+                    message: 'Unknown error'
+                });
+            }
+
+            db.all('SELECT * FROM Rides WHERE rideID = ?', this.lastID, function (err, rows) {
+                if (err) {
+                    return res.send({
+                        error_code: 'SERVER_ERROR',
+                        message: 'Unknown error'
+                    });
+                }
+
+                res.send(rows);
+            });
+        });
+    });
+
+    app.get('/rides', (req, res) => {
+        db.all('SELECT * FROM Rides', function (err, rows) {
+            if (err) {
+                return res.send({
+                    error_code: 'SERVER_ERROR',
+                    message: 'Unknown error'
+                });
+            }
+
+            if (rows.length === 0) {
+                return res.send({
+                    error_code: 'RIDES_NOT_FOUND_ERROR',
+                    message: 'Could not find any rides'
+                });
+            }
+
+            res.send(rows);
+        });
+    });
+
+    app.get('/rides/:id', (req, res) => {
+        db.all(`SELECT * FROM Rides WHERE rideID='${req.params.id}'`, function (err, rows) {
+            if (err) {
+                return res.send({
+                    error_code: 'SERVER_ERROR',
+                    message: 'Unknown error'
+                });
+            }
+
+            if (rows.length === 0) {
+                return res.send({
+                    error_code: 'RIDES_NOT_FOUND_ERROR',
+                    message: 'Could not find any rides'
+                });
+            }
+
+            res.send(rows);
+        });
+    });
+
+    return app;
+};
+
+// Override the base console log with winston
+console.log = function() {
+    return logger.info.apply(logger, arguments)
+}
